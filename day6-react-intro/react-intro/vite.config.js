fae25b4afa1e9b89f3ea7bac2fc1d2b232b91094
rohanmangalxb <rohanmangal.2021@gmail.com>
2025-07-10 17:44:29 +0530

    D

--- day6-react-intro/react-intro/vite.config.js
+++ day6-react-intro/react-intro/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-intro/",
+  base: "/day6-react-intro/react-intro/",
   plugins: [tailwindcss(),react()],
 })
