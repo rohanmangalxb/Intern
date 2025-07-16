233e9de70593ceaaf2619b79b1608837cc2235d9
rohanmangalxb <rohanmangal.2021@gmail.com>
2025-07-16 17:34:33 +0530

    D

--- day9-react-list-map/src/App.jsx
+++ day9-react-list-map/src/App.jsx
@@ -3,19 +3,22 @@ import UserList from './components/userList'
 
 function App() {
 
-    const users = [
-      { id: 1, name: "N", role: "Frontend Intern" },
-      { id: 2, name: "A", role: "Full Stack Intern" },
-      { id: 3, name: "rohan", role: "UI Designer" },
-      { id: 4, name: "riya", role: "UI Designer" },
-      { id: 5, name: "saksham", role: "UI Designer" },
-      { id: 6, name: "bhavesh", role: "intern" },
-      { id: 7, name: "aman", role: "intern" },
-    ];
+  const users = [
+    { id: 1, name: "N", role: "Frontend Intern" },
+    { id: 2, name: "A", role: "Full Stack Intern" },
+    { id: 3, name: "rohan", role: "UI Designer" },
+    { id: 4, name: "riya", role: "UI Designer" },
+    { id: 5, name: "saksham", role: "UI Designer" },
+    { id: 6, name: "bhavesh", role: "intern" },
+    { id: 7, name: "aman", role: "intern" },
+  ];
 
   return (
     <>
-        <UserList users = {users}/>
+      <div className="body">
+
+        <UserList users={users} />
+      </div>
 
     </>
   )
